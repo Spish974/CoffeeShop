fb78ec1029645c0fa33855e0897764ba9669ddbd
Hoareau <f.hoareau@rt-iut.re>
2019-10-21 13:40:27 +0400

    Ajout pages

---
+++ panier.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <div>
+            <li>
+                <a href="index.php">Accueil</a>
+                <a href="menu.php">Menu</a>
+                <a href="panier.php">Panier</a>
+                <a href="compte.php">Compte</a>
+            </li>
+        </div>
+    </header>
+    <head>
+        <title>Coffee Shop</title>
+        <h1>Menu</h1>
+        
+    </head>
+
+
+    <body>
+
+    </body>
+</html>
