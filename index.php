29272605c4562485a33eb07f4100a46dc5eb274e
Lucas Rayan <rlucas974@outlook.fr>
2019-10-21 13:37:25 +0400

    ajout template

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Coffee Shop</title>
+        <meta charset="utf-8" />
+        <link href="src/style.css" rel="stylesheet" />
+        <link href="src/script.js" rel="scriptsheet" />
+    </head>
+    <body>
+        <?php require('footer.php'); ?>
+    </body>
+</html>
