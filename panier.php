f7c72daae369b6fad80845fbe0ef925e2a141d15
Hoareau <f.hoareau@rt-iut.re>
2019-10-21 16:03:51 +0400

    Création des fonctions utiliser dans le panier

--- panier.php
+++ panier.php
@@ -1,7 +1,15 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>Coffee Shop - Panier</title>
+        <meta charset="utf-8" />
+        <link href="src/style.css" rel="stylesheet" />
+        <link href="src/script.js" rel="scriptsheet" />
+        <h1>Panier</h1>
+    </head>
+
     <header>
-        <div>
+        <div class='heading'>
             <li>
                 <a href="index.php">Accueil</a>
                 <a href="menu.php">Menu</a>
@@ -10,14 +18,73 @@
             </li>
         </div>
     </header>
-    <head>
-        <title>Coffee Shop</title>
-        <h1>Menu</h1>
-        
-    </head>
-
-
+    
     <body>
 
+    <div class = 'liste'>
+            
+    </div>
+
+    <div class = 'resume'>
+
+    </div>
+
     </body>
+        <?php
+        $panierExiste = false;
+
+        if($panierExiste = false){ //si le panier n'existe pas on le créer
+            $panierExiste = true; //le panier a été créer
+
+            $panier = array( //le panier contient une liste de produit ainsi que la somme des produits
+                somme => 0,
+            );
+
+        }   else if($panierExiste = true){
+                //print_r($panier);
+                for ($i = 0; $i < $np; $i++) { //boucle affichage contenu panier
+                    nombreProduit();
+                    echo $panier[$i];
+                }
+                function nombreProduit(){ //fonction comptage du nombre de produit dans le panier
+                  $np = count ($panier);
+                }
+                function creeProduit($nom, $prix, $quantite){ //fonction création d'un produit
+                    $panier[$np+1] = array($nom, $prix ,$quantite);
+                }
+                function calculSomme(){
+                    nombreProduit();
+                    $somme = 0;
+                    for ($i=0; $i< $np; $i++){
+                        $sommme = $sommme + $panier[$i][1];
+                    }
+                }
+                function augmenteQuantite($nom){ //fonction ajout d'un même produit
+                    nombreProduit();
+                    for ($i=0; $i< $np; $i++){
+                        if($panier[$i][1] == $nom){
+                            $panier[$i][2] = $panier[$i][2] + 1;
+                        }
+                    }
+                }
+                function reduireQuantite($nom){ //fonction reduire la quantité de produit
+                    nombreProduit();
+                    for ($i=0; $i< $np; $i++){
+                        if($panier[$i][1] == $nom){
+                            $panier[$i][2] = $panier[$i][2] - 1;
+                        }
+                    }
+                }
+                function retire($nom){ //fonction suppression du produit
+                    nombreProduit();
+                    for ($i=0; $i< $np; $i++){
+                        if($panier[$i][0] == $nom){ 
+                            $panier[$i][0] = null;
+                        }
+                    }
+                }
+
+            }
+
+        ?>
 </html>
