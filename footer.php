5cf3f823585276ae8e17279707424772564bb8d1
Lucas Rayan <rlucas974@outlook.fr>
2019-10-22 13:59:52 +0400

    template/page accueil

--- footer.php
+++ footer.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
 <html>
-    <footer>
-        
-    </footer>
+<footer>
+    <h1>Coffee Shop</h1>
+    <div class="cont-1">
+        <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <a href="http://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="http://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+    </div>
+    <div class="cont-2">
+        <a>A propos</a>
+        <a>Conditions</a>
+        <a>Contact</a>
+        <a>Livraison</a>
+    </div>
+    <p class="credit">Par Rayan, Philippe et Adnan - 2019</p>
+</footer>
+
 </html>
--- mainmenu.php
+++ mainmenu.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
-    <div class="mainmenu">
-        <div class="contener">
-            <a href="index.php">Accueil</a>
-            <a href="menu.php">Menu</a>
-            <a href="panier.php"><i class="fas fa-shopping-bag"></i> Panier</a>
-            <a href="compte.php">Compte</a>
-        </div>
+<div class="mainmenu">
+    <div class="titlemenu"><a>Coffee Shop</a></div>
+    <div class="contener">
+        <a href="index.php">Accueil</a>
+        <a href="template.php?page=menu">Menu</a>
+        <a href="template.php?page=panier"><i class="fas fa-shopping-bag"></i> Panier</a>
+        <a href="template.php?page=compte">Compte</a>
     </div>
+</div>
+
 </html>
