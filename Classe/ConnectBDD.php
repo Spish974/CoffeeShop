ea269d39ac4b225d5bfe0ab0d4377af5ab90f7c9
Spish974 <53474519+Spish974@users.noreply.github.com>
2019-10-25 14:41:07 +0400

    menu v1

--- Classe/ConnectBDD.php
+++ Classe/ConnectBDD.php
@@ -12,16 +12,21 @@ class Base
     public function __toString()
     { }
 
-    public function TestClientConnexion($user,$pass)
+    public function TestClientConnexion($user, $pass)
     {
         $stmt = $this->pdo->query("SELECT * FROM `Client` WHERE `Username`='$user' AND `Password`='$pass'");
         if ($stmt->rowCount() == 1) {
             $donnees = $stmt->fetch();
-            if (isset($_SESSION["logconnect"])==false) {
-                $_SESSION["logconnect"]=true;
+            if (isset($_SESSION["logconnect"]) == false) {
+                $_SESSION["logconnect"] = true;
             }
             $_SESSION["nomsession"] = $donnees['Nom'];
             $_SESSION["prenomsession"] = $donnees['Prenom'];
+            $_SESSION["Telephonesession"] = $donnees['Telephone'];
+            $_SESSION["Index_Adressesession"] = $donnees['Index_Adresse'];
+            $_SESSION["Usernamesession"] = $donnees['Username'];
+            $_SESSION["Emailsession"] = $donnees['Email'];
+            $_SESSION["Statutsession"] = $donnees['Statut'];
             return "1";
         } else {
             return "0";
@@ -30,8 +35,23 @@ class Base
     public function Deconnexion()
     {
         if (isset($_SESSION["logconnect"])) {
-        $_SESSION["logconnect"]=false;
+            $_SESSION["logconnect"] = false;
         }
     }
-
+    public function AfficheArticleFilter($filter = "")
+    {
+        if ($filter == "") {
+            $stmt = $this->pdo->query("SELECT * FROM `Produit` WHERE 1");
+            if ($stmt->rowCount() > 0) {
+                while ($donnees = $stmt->fetch()) {
+                    echo "<div class='list-article-unite'><img alt src='src/img/".$donnees["Image"]."'>
+                    <p class='tit-art-unit'>".$donnees["Libellé"]."</p>
+                    <p class='prix-art-unit'>".$donnees["Prix"]."€</p>
+                    </div>";
+                }
+            }
+        } else {
+            echo "qsd";
+        }
+    }
 }
--- popuplogin.php
+++ popuplogin.php
@@ -1,22 +1,27 @@
 <?php
-if(isset($_SESSION["logconnect"])==false){
-?>
-<div class=<?php echo '"popup-login';
-if(isset($_POST["action"])){}else{echo " act hind";}
-echo '"'?>>
-    <div class="popform">
-        <div class="close"><i class="far fa-times-circle"></i></div>
-        <h1 class="titrepopup">Login</h1>
+if (isset($_SESSION["logconnect"]) == false) {
+    ?>
+    <div class=<?php echo '"popup-login';
+                    if (isset($_POST["action"])) { } else {
+                        echo " act hind";
+                    }
+                    echo '"' ?>>
+        <div class="popform">
+            <div class="close"><i class="far fa-times-circle"></i></div>
+            <h1 class="titrepopup">Login</h1>
 
-        <div class="saisie">
-            <form action="" method="POST">
-                <input type="hidden" name="action" value="log">
-                <input type="text" name="usrname" id="email" size="30" required="">
-                <input type="password" name="password">
-                <input type="submit" value="Login">
-            </form>
+            <div class="saisie">
+                <form action="" method="POST">
+                    <input type="hidden" name="action" value="log">
+                    <div class="champsaisie">
+                        <input type="text" name="usrname" id="email" size="30" required="" placeholder="Identifiant">
+                        <input type="password" name="password" placeholder="Mot de passe">
+                    </div>
+                    <input type="submit" value="Connecter" class="butt-log">
+                </form>
+                <a class="inscription">Inscription</a>
+            </div>
         </div>
     </div>
-</div>
 <?php
-}?>
+} ?>
