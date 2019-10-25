aaa4c4159d31642844fc8b85f89451bdf2af0d08
Spish974 <53474519+Spish974@users.noreply.github.com>
2019-10-25 15:23:58 +0400

    aze

--- menu.php
+++ menu.php
@@ -1,4 +1,6 @@
 <?php
     $bdd= new Base();
     $bdd->AfficheArticleFilter();
+    
 ?>
+<a id="drapeau_NL" onclick="<?php echo "aze"; ?>">dfg</a>
--- template.php
+++ template.php
@@ -7,22 +7,29 @@
     if (isset($_GET['page'])) {
         $page = $_GET['page'];
     }
-    if ($page == "menu" || $page == "panier" || $page == "compte" || $page == "admin") {echo "<title>" . $page . "</title>";} else {header('Location: index.php');
-        exit();}
+    if ($page == "menu" || $page == "panier" || $page == "compte" || $page == "admin") {
+        echo "<title>" . $page . "</title>";
+    } else {
+        header('Location: index.php');
+        exit();
+    }
     ?>
-    <?php require("head.php");?>
+    <?php require("head.php"); ?>
 </head>
 
 <body style="background-color: #ffffff;" class="template">
     <?php require('mainmenu.php'); ?>
     <?php
-    if ($page !="1") {
+    if ($page != "1") {
         echo "<title>" . $page . "</title>";
         echo "<div class='titrepage'><h1>" . $page . "</h1></div>";
         echo "<div class='contenu' style='margin-bottom:50vh;'>";
         require($page . '.php');
         echo "</div>";
-    } else {echo "<p>aucune page trouver</p>";}?>
+    } else {
+        echo "<p>aucune page trouver</p>";
+    } ?>
 </body>
 <?php require('footer.php'); ?>
+
 </html>
