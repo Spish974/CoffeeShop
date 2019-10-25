783de18ff63f8618b4371bf9bb50c8c7ebb8f0ca
adnan974 <56817528+adnan974@users.noreply.github.com>
2019-10-25 14:45:11 +0400

    admin page modifs

---
+++ admin.php
@@ -0,0 +1,35 @@
+<div class="page_admin">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg">
+                <!--<a class="btn btn-info btn-lg" href="template.php?page=AfficheClient" type="button">Afficher client</a>-->
+                <a href="template.php?page=AfficheClient"><button type="button" class="btn btn-primary btn-lg btn-block "> Client </button></a>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <a href="template.php?page=AfficheClient">
+                <div class="col-lg">
+                    <button type="button" class="btn btn-primary btn-lg btn-block "> Produit </button>
+                </div>
+            </a>
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <a href="template.php?page=AfficheClient">
+                
+                <button type="button" class="btn btn-primary">test</button>
+                
+            </a>
+        </div>
+    </div>
+
+
+    
+
+</div>
