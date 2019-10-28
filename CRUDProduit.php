c9dd1116bf9cbaadffc1a5e5d1004543e58cdd4e
Spish974 <53474519+Spish974@users.noreply.github.com>
2019-10-28 18:35:38 +0400

    correction affichage

--- CRUDProduit.php
+++ CRUDProduit.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,65 +6,58 @@
 
     <title>Affichage base client <br /></title>
 </head>
+<br /><br />
+<h2> Base produit actuelle <br /><br /><br /></h2>
 
-<body>
-    <br /><br />
-    <h2> Base produit actuelle <br /><br /><br /></h2>
+<?php
 
-    <?php
+$dsn = 'mysql:host=mysql-rlucas974.alwaysdata.net;dbname=rlucas974_coffeeshop;port=3306;charset=utf8';
 
-    $dsn = 'mysql:host=mysql-rlucas974.alwaysdata.net;dbname=rlucas974_coffeeshop;port=3306;charset=utf8';
+$pdo = new PDO($dsn, 'rlucas974', 'm9741223');
 
-    $pdo = new PDO($dsn, 'rlucas974', 'm9741223');
-
-    $stmt = $pdo->query("SELECT `ID_Produit`,`Libellé`,Prix FROM `Produit`");
+$stmt = $pdo->query("SELECT `ID_Produit`,`Libellé`,Prix FROM `Produit`");
 
 
-    ?>
-    <div class="container">
-        <div class="col-lg-12">
-            <table class="table table-bordered">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Référence</th>
-                        <th>Libellé</th>
-                        <th>Prix</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-
-                <?php
-                while ($donnee = $stmt->fetch()) {
-                    ?>
-
-                    <tbody>
-                        <tr>
-
-                            <td scope="row"> <?php echo ($donnee["ID_Produit"]) ?> </td>
-                            <td> <?php echo ($donnee["Libellé"]) ?> </td>
-                            <td> <?php echo ($donnee["Prix"]) ?> </td>
-                            <td> <?php echo "<a class='btn btn-primary' href='ModifProduit.php?id=" . $donnee['ID_Produit'] . "'> Modifier </a>"; ?>
-                                <?php echo "<a class='btn btn-primary' href='SupprimeProduit.php?id_produit=" . $donnee['ID_Produit'] . "'> Supprimer </a>"; ?>
-                            </td>
-                        </tr>
-                    </tbody>
-                <?php } ?>
-            </table>
-            <div>
-            </div>
-            <br /><br /><br />
-
-            <a name="AjouteProduit" id="" class="btn btn-primary" href="AjouteProduit.php" role="button">Ajouter un produit</a>
+?>
+<div class="container">
+    <div class="col-lg-12">
+        <table class="table table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Référence</th>
+                    <th>Libellé</th>
+                    <th>Prix</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
 
             <?php
-            //$transfert = $pdo->query("INSERT INTO `Produit`(`ID_Produit`, `Libellé`, `Prix`, `Description`, Catégorie) VALUES ('123456','test','1.99','testestest','testcatégorie')"); 
-            //$req_transfert = INSERT INTO `Produit`(`Description`, `Libellé`) VALUES ('testestest1', 'testtest2')""; 
-            //exec($req_transfert);
-            ?>
+            while ($donnee = $stmt->fetch()) {
+                ?>
 
+                <tbody>
+                    <tr>
 
+                        <td scope="row"> <?php echo ($donnee["ID_Produit"]) ?> </td>
+                        <td> <?php echo ($donnee["Libellé"]) ?> </td>
+                        <td> <?php echo ($donnee["Prix"]) ?> </td>
+                        <td> <?php echo "<a class='btn btn-primary' href='ModifProduit.php?id=" . $donnee['ID_Produit'] . "'> Modifier </a>"; ?>
+                            <?php echo "<a class='btn btn-primary' href='SupprimeProduit.php?id_produit=" . $donnee['ID_Produit'] . "'> Supprimer </a>"; ?>
+                        </td>
+                    </tr>
+                </tbody>
+            <?php } ?>
+        </table>
+        <div>
+        </div>
+        <br /><br /><br />
 
+        <a name="AjouteProduit" id="" class="btn btn-primary" href="AjouteProduit.php" role="button">Ajouter un produit</a>
 
-</body>
-
-</html>
+        <?php
+        //$transfert = $pdo->query("INSERT INTO `Produit`(`ID_Produit`, `Libellé`, `Prix`, `Description`, Catégorie) VALUES ('123456','test','1.99','testestest','testcatégorie')"); 
+        //$req_transfert = INSERT INTO `Produit`(`Description`, `Libellé`) VALUES ('testestest1', 'testtest2')""; 
+        //exec($req_transfert);
+        ?>
+    </div>
+</div>
--- admin.php
+++ admin.php
@@ -1,8 +1,5 @@
 <div class="page_admin">
-
     <div class="container">
-
-        <p><br /></p>
         <div class="row">
             <div class="col-lg-4">
                 <!--<a class="btn btn-info btn-lg" href="template.php?page=AfficheClient" type="button">Afficher client</a>-->
@@ -13,16 +10,6 @@
                 <!--<a class="btn btn-info btn-lg" href="template.php?page=AfficheClient" type="button">Afficher client</a>-->
                 <a href="template.php?page=CRUDProduit"><button type="button" class="btn btn-primary btn-lg btn-block "> Produit </button></a>
             </div>
-
         </div>
-
-        
-            
-        
-
     </div>
-
-
-
-
 </div>
